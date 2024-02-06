32133b18fedd2e5b0e0e206a84af5ea0b77ac6c0
joaogabsouza <144790791+Joaogabsouza@users.noreply.github.com>
2024-02-05 21:11:25 -0300

    Alteração: Opção de saída inserida
    incluso no sistema a opção de saída no menu, para o usuário.

--- Projeto.c
+++ Projeto.c
@@ -152,6 +152,7 @@ int main ()
 	printf ("\t1- Registrar nomes\n");
 	printf ("\t2- Consultar nomes\n");
 	printf ("\t3- Deletar nomes\n");
+	printf ("\t4- Sair do sistema\n");
 	printf ("Opção:");
 	
 	scanf ("%d", &opcao);
@@ -171,6 +172,11 @@ int main ()
 	
 	case 3:
 		deletar ();
+	break;
+	
+	case 4:
+		printf("\n\nObrigado por utilizar o sistema.\n");
+		return 0;
 	break;
 	
 	default:
